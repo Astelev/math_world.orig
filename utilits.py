de450cd282d5a27d4ce820567ec7ad79307ff932
AEgorov-2022 <yegorov_andrey@mail.ru>
2023-12-25 12:55:48 +0300

    language fix

--- utilits.py
+++ utilits.py
@@ -11,25 +11,26 @@ def load_image(name):
     image = pygame.image.load(fullname)
     return image
 
+
 class Anim:
     def __init__(self, filename, frames):
         self.images = []
         for i in range(frames):
             name = os.path.join('anime', filename, (str(i) + ".png"))
             self.images.append(load_image(name))
-        self.nuwframe = 0
-    def framedrow(self, screen, x, y):
-        screen.blit(self.images[self.nuwframe], (x, y))
-        if self.nuwframe < len(self.images) - 1:
-            self.nuwframe += 1
+        self.nowframe = 0
+
+    def framedraw(self, screen, x, y):
+        screen.blit(self.images[self.nowframe], (x, y))
+        if self.nowframe < len(self.images) - 1:
+            self.nowframe += 1
         else:
-            self.nuwframe = 0
+            self.nowframe = 0
 
 
-
-class Word:
-    def __init__(self, camerax, cameray, screen, objects=[], florcul=(100, 100, 100), backcol=(0, 0, 0)):
-        self.florcul = florcul
+class World:
+    def __init__(self, camerax, cameray, screen, objects=[], florcol=(100, 100, 100), backcol=(0, 0, 0)):
+        self.florcol = florcol
         self.backcol = backcol
         self.camx = camerax
         self.camy = cameray
@@ -44,8 +45,8 @@ class Word:
         if self.return_obj("person").do[0] == "jump":
             self.jump()
         for i in self.col:
-                i.display(int(self.camx), int(self.camy), self.scr)
-        pygame.draw.rect(self.scr, self.florcul, (0, 10-self.camy, 1000, 1000))
+            i.display(int(self.camx), int(self.camy), self.scr)
+        pygame.draw.rect(self.scr, self.florcol, (0, 10 - self.camy, 1000, 1000))
 
     def jump(self, start=False):
         do = self.return_obj("person").do
@@ -56,7 +57,7 @@ class Word:
             do[0] = ""
             do[1] = 0
 
-    def return_obj(self, name = "", number = -1):
+    def return_obj(self, name="", number=-1):
         if name != "":
             for i in self.col:
                 if i.name == name:
@@ -66,21 +67,23 @@ class Word:
         else:
             return self.col
 
-    def click(self, x, y, muveobj=False):
+    def click(self, x, y, moveobj=False):
         for i in self.col:
             if i.x < x + self.camx < i.x + i.size and i.y < y + self.camy < i.y + i.size:
                 return i
         return False
 
+
 class Number:
-    def __init__(self, count, x, y, size = 50, name = "", movable=True):
-        self.muve = movable
+    def __init__(self, count, x, y, size=50, name="", movable=True):
+        self.move = movable
         self.c = count
         self.name = name
         self.x = x
         self.y = y
         self.size = size
         self.do = ["", 0]
+
     def display(self, x, y, screen):
         font = pygame.font.Font(None, self.size)
         text = font.render(str(self.c), True, (255, 255, 255))
@@ -91,11 +94,12 @@ class Number:
         self.y = camy + y - self.size // 2
         self.do[0] = "move"
 
+
 class Person:
-    def __init__(self, size = 50, name = ""):
+    def __init__(self, size=50, name=""):
         self.image = load_image("person.png")
         self.run = Anim("run", 2)
-        self.muve = False
+        self.move = False
         self.name = name
         self.x = 0
         self.y = 0
@@ -111,6 +115,6 @@ class Person:
 
     def display(self, x, y, screen):
         if self.do[0] == "move":
-            self.run.framedrow(screen, 400, 310)
+            self.run.framedraw(screen, 400, 310)
         else:
             screen.blit(self.image, (400, 310))
