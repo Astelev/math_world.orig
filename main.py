ba090c95790f267dc0cf32086916d806098406f2
liona <lioonalexandrov@gmail.com>
2023-12-25 11:08:02 +0700

    start

---
+++ main.py
@@ -0,0 +1,46 @@
+import pygame
+import os
+import sys
+from utilits import Word , Number, Person, load_image, Anim
+
+
+if __name__ == '__main__':
+    # инициализация Pygame:
+    pygame.init()
+    size = 1000, 800
+    screen = pygame.display.set_mode(size)
+    screen.fill((0, 0, 0))
+    clock = pygame.time.Clock()
+    running = True
+    word = Word(0, -510, screen)
+    word.create_object(Person(name="person"))
+    word.create_object(Number(10, 10, -30))
+    person = word.return_obj("person")
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.MOUSEMOTION:
+                if event.buttons[0]:
+                    x, y = event.pos
+                    returnd = word.click(x, y, True)
+                    if returnd:
+                        returnd.move(x, y, word.camx, word.camy)
+            if event.type == pygame.KEYDOWN:
+                if event.key == 32:
+                    person.status_set("jump")
+        word.display()
+        kays = pygame.key.get_pressed()
+        if kays[pygame.K_a]:
+            word.camx -= 5
+            if person.do[0] != "jump":
+                person.status_set("move")
+        elif kays[pygame.K_d]:
+            word.camx += 5
+            if person.do[0] != "jump":
+                person.status_set("move")
+        else:
+            person.status_set("move", False)
+        pygame.display.flip()
+        clock.tick(60)
+    pygame.quit()
---
+++ utilits.py
@@ -0,0 +1,116 @@
+import pygame
+import os
+import sys
+
+
+def load_image(name):
+    fullname = os.path.join('data', name)
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    return image
+
+class Anim:
+    def __init__(self, filename, frames):
+        self.images = []
+        for i in range(frames):
+            name = os.path.join('anime', filename, (str(i) + ".png"))
+            self.images.append(load_image(name))
+        self.nuwframe = 0
+    def framedrow(self, screen, x, y):
+        screen.blit(self.images[self.nuwframe], (x, y))
+        if self.nuwframe < len(self.images) - 1:
+            self.nuwframe += 1
+        else:
+            self.nuwframe = 0
+
+
+
+class Word:
+    def __init__(self, camerax, cameray, screen, objects=[], florcul=(100, 100, 100), backcol=(0, 0, 0)):
+        self.florcul = florcul
+        self.backcol = backcol
+        self.camx = camerax
+        self.camy = cameray
+        self.col = objects
+        self.scr = screen
+
+    def create_object(self, object):
+        self.col.append(object)
+
+    def display(self):
+        self.scr.fill(self.backcol)
+        if self.return_obj("person").do[0] == "jump":
+            self.jump()
+        for i in self.col:
+                i.display(int(self.camx), int(self.camy), self.scr)
+        pygame.draw.rect(self.scr, self.florcul, (0, 10-self.camy, 1000, 1000))
+
+    def jump(self, start=False):
+        do = self.return_obj("person").do
+        if do[1] <= 120:
+            self.camy = self.camy + (do[1] - 60) / 10
+            do[1] = do[1] + 1
+        elif do[1] > 120:
+            do[0] = ""
+            do[1] = 0
+
+    def return_obj(self, name = "", number = -1):
+        if name != "":
+            for i in self.col:
+                if i.name == name:
+                    return i
+        elif i > -1:
+            return self.col[number]
+        else:
+            return self.col
+
+    def click(self, x, y, muveobj=False):
+        for i in self.col:
+            if i.x < x + self.camx < i.x + i.size and i.y < y + self.camy < i.y + i.size:
+                return i
+        return False
+
+class Number:
+    def __init__(self, count, x, y, size = 50, name = "", movable=True):
+        self.muve = movable
+        self.c = count
+        self.name = name
+        self.x = x
+        self.y = y
+        self.size = size
+        self.do = ["", 0]
+    def display(self, x, y, screen):
+        font = pygame.font.Font(None, self.size)
+        text = font.render(str(self.c), True, (255, 255, 255))
+        screen.blit(text, (self.x - x, self.y - y))
+
+    def move(self, x, y, camx, camy):
+        self.x = camx + x - self.size // 2
+        self.y = camy + y - self.size // 2
+        self.do[0] = "move"
+
+class Person:
+    def __init__(self, size = 50, name = ""):
+        self.image = load_image("person.png")
+        self.run = Anim("run", 2)
+        self.muve = False
+        self.name = name
+        self.x = 0
+        self.y = 0
+        self.size = size
+        self.do = ["", 0]
+
+    def status_set(self, do, status=True):
+        if status:
+            self.do[0] = do
+        elif self.do[0] == do and not status:
+            self.do[0] = ""
+            self.do[1] = 0
+
+    def display(self, x, y, screen):
+        if self.do[0] == "move":
+            self.run.framedrow(screen, 400, 310)
+        else:
+            screen.blit(self.image, (400, 310))
